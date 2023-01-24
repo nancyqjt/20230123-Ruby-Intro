9f7abb1abfe765b9619e9b7a41832e87656c20b2
Nancy Qian <nancyqian2023@u.northwestern.edu>
2023-01-24 02:27:58 +0000

    4-hashes code along

--- code-along/4-hashes.rb
+++ code-along/4-hashes.rb
@@ -3,7 +3,21 @@
 # ruby 4-hashes.rb
 
 # Hashes are lists of *key-value pairs*
+profile = {
+    "name" => "Ben",
+    "location" => "Chicago",
+    "status" => "Teaching",
+}
 
 # Accessing data from the hash
+puts profile ["name"]
+puts profile ["location"]
+puts profile ["age"]
 
 # More Complex Hashes
+
+complete_profile = { "name" => "Ben Block",
+                     "location" => { "city" => "Chicago", "state" => "Illinois" },
+                     "timeline" => [{ "status" => "Eating tacos", "posted" => "7:30am" },
+                                    { "status" => "Brushing teeth", "posted" => "8:00am" },
+                                    { "status" => "Eating more tacos", "posted" => "9:00am" }] }
