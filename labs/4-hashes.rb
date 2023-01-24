5b3763775cb0413cc5aed06e51fc4675a6c8db69
Nancy Qian <nancyqian2023@u.northwestern.edu>
2023-01-24 02:43:54 +0000

    4-hashes complete

--- labs/4-hashes.rb
+++ labs/4-hashes.rb
@@ -30,3 +30,8 @@ bitcoin = bitcoin.to_f
 
 # 3. inspect the bitcoin_data hash
 # puts bitcoin_data
+rate = bitcoin_data ["bpi"]["USD"]["rate_float"]
+value = bitcoin * rate
+
+puts "1 Bitcoin is valued at $#{rate} USD."
+puts "Your Bitcoin is worth $#{value}."
